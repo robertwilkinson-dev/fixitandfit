96ced1218d290ba62cbf5b213db8a7dee7481185
Robert Wilkinson <robert.wilkinson@and.digital>
2024-07-19 00:48:10 +0100

    add bg image

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Fixit & fit</h1>
-      <p>Under Construction</p>
+      <h1>fixit & fit</h1>
+      <p>We're currently under construction, check back soon!</p>
     </main>
   );
 }
